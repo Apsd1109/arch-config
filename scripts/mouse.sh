7da46485e0fdd602a01e788b453b54f45a710d26
apsd1109 <apsd1109@hotmail.ca>
2020-08-17 16:18:52 -0400

    add dark theme, mute function, cleanup

--- scripts/mouse.sh
+++ scripts/mouse.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#turn on BT and connect to mouse
+bluetoothctl power on
+bluetoothctl connect E8:80:60:40:BA:D8
+
 #set mouse sens
 Mouse=`xinput list | grep -i 'MX Master 2S Mouse' | awk -F= '{print $2}' | awk '{print$1}'`
 xinput set-prop $Mouse "libinput Accel Speed" -0.6
--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -1,5 +1,2 @@
 rofi -show run \
 -lines 5 \
--eh 1 \
--font "SFNS Display 18" \
-#-color-normal "#2f343f,#f3f4f5,#2f343f,#2f343f,#9575cd" \
